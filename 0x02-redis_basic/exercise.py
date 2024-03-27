e37a0a59ae90795b83bad0997acbe6bf0702b482
Olisajioke <chijioke914@gmail.com>
2024-03-27 12:36:03 +0100

    task lintint

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -63,9 +63,16 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
-        """Retrieves data from Redis with the specified key and applies the conversion function if provided."""
+     def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        """Retrieves data from Redis with the specified key 
+        and applies the conversion function if provided."""
         data = self._redis.get(key)
         if data is None:
             return None
@@ -74,9 +81,11 @@ class Cache:
         return data
 
     def get_str(self, key: str) -> str:
-        """Retrieves data from Redis with the specified key and returns it as a UTF-8 decoded string."""
+        """Retrieves data from Redis with the specified key and
+        returns it as a UTF-8 decoded string."""
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> int:
-        """Retrieves data from Redis with the specified key and returns it as an integer."""
+        """Retrieves data from Redis with the specified key
+        and returns it as an integer."""
         return self.get(key, fn=int)
